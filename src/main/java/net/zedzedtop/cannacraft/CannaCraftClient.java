3778061746143e2b0d437dd4de3539a0132e8775
Zer0 <dextermovies@yahoo.com>
2023-08-10 13:38:54 -0400

    FirstCommit

---
+++ src/main/java/net/zedzedtop/cannacraft/CannaCraftClient.java
@@ -0,0 +1,10 @@
+package net.zedzedtop.cannacraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CannaCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
